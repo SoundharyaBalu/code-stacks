4ad0472c621573da48bfc52ebaa694be4c20baf8
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-06-07 18:16:54 +0530

    Create repeat_hello.java

---
+++ repeat_hello.java
@@ -0,0 +1,11 @@
+class repeat_hello
+{
+    public static void main(String args[])
+    {
+        String s="Hello";
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(s);
+        }
+    }
+}
