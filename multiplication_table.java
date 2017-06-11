6419ea767e07ae1c95ed52db8c50a13299732f9b
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-06-11 11:43:16 +0530

    Create multiplication_table.java

---
+++ multiplication_table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class multiplication_table
+{
+public static void main(Strin args[])
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+for(int i=1;i<=10;i++)
+{
+System.out.println(n+"*"+i+"="(n*i));
+}
+}
+}
