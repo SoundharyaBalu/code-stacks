12bf96de64606b3887776d0974670103c8633ad7
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-06-29 17:10:57 +0530

    Create fav_number.java

---
+++ fav_number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class fav_number
+{
+public static void main(String args)
+{
+Scanner sc=new Scanner(String args[])
+String s=sc.next();
+String ss[]=new String[4];
+int m=5,n=10,x=0,y=0;
+while(n<=s.length())
+{
+ss[]=substring(m,n).split("");
+x=0;
+for(int i=0;i<4;i++)
+{
+x=x+Integer.parseInt(ss[i]);
+}
+while(x>9)
+{
+y=0;
+for(int i=0;x!=0;i++)
+{
+y=y+(x%10);
+x=x;
+}
+}
+}
+}
+}
