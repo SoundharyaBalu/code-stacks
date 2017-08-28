c256c60381129a23bc980dae8b08bcdf1bc79ab2
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-08-28 19:01:40 +0530

    Create first_repeat.java

---
+++ first_repeat.java
@@ -0,0 +1,18 @@
+import java.io.*;
+class first_repeat
+{
+public static void main(String args[])
+{
+int arr[]=new int[args.length];
+for(int i=0;i<args.length;i++)
+{
+arr[i]=Integer.parseInt(args[i]);
+}
+for(int i=0;i<a.length;i++)
+{
+if(arr[i]==arr[i+1]){
+System.out.println(arr[i]);
+}
+}
+}
+}
