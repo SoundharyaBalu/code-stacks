44116ba44aa19ecbcf5ddcde306c98bee52aa530
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-08-28 19:46:29 +0530

    Create Kth_tallest.java

---
+++ Kth_tallest.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Kth_tallest
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int a[]=new int[50];
+    for(int i=0;i<50;i++){
+    a[i]=sc.nextInt();
+    }
+    System.out.println("4th Tallest : "+a[a.length-4]);
+    int k=sc.nextInt();
+    if(k<50&&k>0)
+    {
+        System.out.println(k+"th Tallest : "+arr[arr.length-k]);
+    }
+    else
+    {
+    System.out.println("There is no "+k+"th Student in given list");
+    }
+  }
+}
