8e086a3b466d910a2b65d489c1fe545df3df1bd7
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-09-12 21:25:33 +0530

    Create str_int.java

---
+++ str_int.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class str_int {
+	public static void main(String[] args) {
+		Scanner	in=new Scanner(System.in);
+String sc=in.nextLine();
+int a=Integer.parseInt(sc);
+System.out.println(a);
+	}
+
+}
