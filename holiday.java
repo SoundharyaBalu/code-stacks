ab36c827572e91030d13c8b6007f79a117fb3f8a
SOUNDHARYA B <soundharyabalu4297@gmail.com>
2017-10-02 18:42:23 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class holiday
+{
+public static void main(String args[])
+{
+if(args[0].equalsIgnoreCase("sunday"))
+{
+System.out.print("False");
+}
+else
+System.out.print("True");
+}
+}
